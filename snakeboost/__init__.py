06f9c161dd5c8c6224e3461799809fa7c26a4bd6
Peter Van Dyken <pvandyk2@uwo.ca>
2021-11-30 10:40:41 -0500

    Handle __init__ files with mkinit

--- snakeboost/__init__.py
+++ snakeboost/__init__.py
@@ -1,18 +1,11 @@
-__submodules__ = ["tar", "pipenv", "xvfb"]
-
-__ignore__ = ["PYTHON_VENV_CREATE_ERR", "TIMED_OUT_ERR"]
+__submodules__ = ["tar", "pipenv", "xvfb", "utils"]
 
 # <AUTOGEN_INIT>
-from snakeboost.tar import (
-    Tar,
-)
-from snakeboost.pipenv import (
-    PipEnv,
-)
-from snakeboost.xvfb import (
-    XvfbRun,
-)
+from snakeboost.pipenv import PipEnv
+from snakeboost.tar import Tar
+from snakeboost.utils import pipe
+from snakeboost.xvfb import XvfbRun
 
-__all__ = ["PipEnv", "Tar", "XvfbRun"]
+__all__ = ["PipEnv", "Tar", "XvfbRun", "pipe"]
 
 # </AUTOGEN_INIT>
