acf1df8b2623f76ce701fb9416d2ba4b86482d5c
Peter Van Dyken <pvandyk2@uwo.ca>
2022-01-13 17:20:06 -0500

    Add documentation for Pyscript

--- README.md
+++ README.md
@@ -78,6 +78,144 @@ Environment creation is threadsafe, so having 32 rules using the same environmen
 
 [Source](snakeboost/pipenv.py)
 
+### `Pyscript`
+
+Because Pipenv modifies the shell script, it cannot be used with Snakemake's `script` directive.
+`Pyscript` fills this gap, providing similar features to the native `script` directive while remaining compatible with Pipenv.
+It can also be combined with any of the other enhancers!
+
+Pyscript works by calling your script as a command line progam, passing inputs, outputs, params, and every other snakemake entity as arguments.
+On the Snakefile side, snakeboost provides the `Pyscript` enhancer, which composes the bash to call your python file.
+On the script side, snakeboost has the `snakemake_parser()` function, which automatically parses the command line arguments sent by Snakemake and organizes them into the `SnakemakeArgs` class.
+This object has the same interface as the `snakemake` object used in ordinary `snakemake` scripts; the only difference is that your script must retrieve it by importing and calling `snakemake_parser().
+Thus, any scripts called by `Pyscript` must have `snakeboost` as a dependency in their Python environment.
+
+`Pyscript` will attempt to automatically pass on every entity (including inputs, outputs, params, etc) in your rule on to the script.
+This works fantastic for single items or lists:
+
+```python
+rule some_rule:
+    input: "path/to/input"
+
+rule rule_with_list_input:
+    input:
+        "/path/1",
+        "/path/2",
+        "/path/3"
+```
+
+These will be received in your script as:
+
+```python
+
+from snakeboost import snakemake_parser
+
+snakemake = snakemake_parser()
+
+# some_rule
+assert snakemake.input == Path("path/to/input")
+
+# rule_with_list_input
+assert snakemake.input == [
+    Path("/path/1"),
+    Path("/path/2"),
+    Path("/path/3")
+]
+```
+
+Unfortunately, it is unable to infer the names of named entities:
+
+```python
+rule named_inputs:
+    input:
+        input_1="/path/1",
+        input_2="/path2
+```
+
+By default, these will be treated as lists.
+You can, however, annotate your rules to inform `Pyscript` of the entity names.
+It can then pass them on to `snakemake_parser()` to form dicts.
+
+There are two methods to achieve this.
+The first is through inline annotation:
+
+```python
+my_script = Pyscript(workflow.basedir)
+rule rule_with_script:
+    input:
+        **my_script.input(
+            input_1="path/1",
+            input_2="path/2
+        )
+    output: "path/to/output
+    shell: my_script("scripts/my_script.py")
+```
+
+Here, we start by defining the script before the rule.
+Notice how `workflow.basedir` is passed into `Pyscript` as an argument.
+This is a variable provided by Snakemake set to the location of the Snakefile.
+We use it here to inform `Pyscript` where the scripts directory will be.
+Second, we wrap named entities using the appropriate method from our `Pyscript` instance (we would use `my_method.params()` for params, etc).
+Notice the double asterisk `**` before the method call.
+This is essential to properly unpack the Dictionary returned by `.input()` so it can be read by Snakemake.
+Finally, we call our `Pyscript` instance with the path of our script relative to the Snakefile (just like in normal Snakemake scripts) and pass it to the **shell** directive.
+Notice how we only need to wrap named entities.
+In the above example, `output` will still be passed on to the script.
+
+The second method is explicately listing the entities we'd like to include as parameters in our final call to the `Pyscript` instance:
+
+```python
+rule rule_with_script:
+    input:
+        input_1="path/1",
+        input_2="path/2",
+        input_3="path/3
+    output: "path/to/output
+    shell:
+        Pyscript(workflow.basedir)(
+            "scripts/my_script.py",
+            input=["input_1", "input_2"]
+        )
+```
+
+This method has the advantage of only passing the explicitely named entities.
+In the above example, `input_1` and `input_2` would be passed to the script, but not `input_3`.
+Note that other entity types, such as output in the above example, would be passed as normal.
+The method also allows a completely inline creation of the `Pyscript`, which may be preferable.
+
+Whichever method you choose, named entities can be consumed in your script as follows:
+
+```python
+
+from snakeboost import snakemake_parser
+
+snakemake = snakemake_parser()
+
+assert snakemake.input == {
+    "input_1": Path("path/1")
+    "input_2": Path("path/2")
+}
+```
+
+Finally, `Pyscript` is easily combined with a pipenv by supplying the env object as an argument to the `Pyscript` constructor:
+
+```python
+my_env = Pipenv("/tmp", packages=["snakeboost"])
+my_script = Pyscript(workflow.basedir, my_env)
+```
+
+Note that `Pyscript` and `Pipenv` must be combined in the above way.
+`Pyscript` should not be nested under a call of a `Pipenv` method:
+
+```python
+# Don't do this!! You'll just get an error.
+rule mistake:
+    shell:
+        my_env.python(
+            Pyscript(workflow.basedir)("scripts/my_script.py")
+        )
+```
+
 ### `Tar`
 
 Sometimes, rules produce a directory full of files as output.
@@ -165,4 +303,3 @@ Then clone the repo and initialize by running:
 poetry install
 poetry run pre-commit install
 ```
-
--- snakeboost/script.py
+++ snakeboost/script.py
@@ -32,12 +32,68 @@ def _get_arg(arg: str, value: Optional[PyscriptParam]):
     return f"--{arg} " + _mapping(arg, value)
 
 
-# pylint: disable=redefined-builtin, missing-class-docstring
-# pylint: disable=attribute-defined-outside-init
+# pylint: disable=redefined-builtin, attribute-defined-outside-init
 @attr.define(slots=False)
 class Pyscript:
+    """Functions to run python scripts
+
+    Runs python scripts similarly to the script directive in Snakemake, but can be used
+    with Snakeboost `PipEnv`s. Like the script directive, inputs, outputs, params, and
+    any other Snakemake data can be passed to the script.
+
+    Pyscript can be combined with any other snakeboost function. It should take the
+    place of the bash script. It should never be directly combined with Pipenv, however.
+    Instead, supply the Pipenv to the venv parameter when initializing the Pyscript.
+
+    Currently, only items serializable as strings can be provided. This includes text,
+    numbers, Paths, etc. Complex objects may be supported in the future.
+
+    The data will be provided to the script via SnakemakeArgs.
+
+    Example
+    -------
+    To preserve named data, such as:
+
+    ```
+    input:
+        first="/path/to/first",
+        second="/path/to/second"
+    ```
+
+    annotations must be provided. There are two methods for this. The first is to
+    provide the names of data when calling the script. See the __call__() method for
+    more details. The second is to annotate each type of data when defining it in
+    the rule using the corresponding Pyscript method. For example:
+
+    ```
+    pyscript = Pyscript(snakemake_dir)
+    rule rule_name:
+        input:
+            **pyscript.input(
+                first="/path/to/first",
+                second="/path/to/second"
+            )
+        shell:
+            pyscript("scripts/script_to_run.py")
+    ```
+
+    Here, the two inputs would be passed to the script under the names "first" and
+    "second". Note that the double asterisk (**) is NECESSARY to properly unpack the
+    dict returned by the method.q
+
+    Parameters
+    ----------
+    snakefile_dir : Path or str
+        Path to the snakemake app directory or Snakefile directory. This, combined with
+        the script path provided later, should form a fully resolved path to the script,
+        e.g. snakefile_dir/script_path.py
+
+    venv : PipEnv, optional
+        PipEnv with which to call the script
+    """
+
     snakefile_dir: Path = attr.ib(converter=Path)
-    env: Optional[PipEnv] = None
+    venv: Optional[PipEnv] = None
 
     def __attrs_post_init__(self):
         self._input = None
@@ -48,26 +104,88 @@ class Pyscript:
         self._log = None
 
     def input(self, **kwargs):
+        """Set named inputs to the pyscript
+
+        Wrap this function around your rule inputs. Be sure to include a double
+        asterisk before the function to unpack the dict, e.g. `**pyscript.input(...)`
+
+        Returns
+        -------
+        Dict
+            Dict of name, value pairs. This should be unpacked using a double asterisk
+        """
         self._input = ScriptDict(**kwargs)
         return kwargs
 
     def output(self, **kwargs):
+        """Set named outputs to the pyscript
+
+        Wrap this function around your rule outputs. Be sure to include a double
+        asterisk before the function to unpack the dict, e.g. `**pyscript.output(...)`
+
+        Returns
+        -------
+        Dict
+            Dict of name, value pairs. This should be unpacked using a double asterisk
+        """
         self._output = ScriptDict(**kwargs)
         return kwargs
 
     def params(self, **kwargs):
+        """Set named params to the pyscript
+
+        Wrap this function around your rule params. Be sure to include a double
+        asterisk before the function to unpack the dict, e.g. `**pyscript.params(...)`
+
+        Returns
+        -------
+        Dict
+            Dict of name, value pairs. This should be unpacked using a double asterisk
+        """
         self._params = ScriptDict(**kwargs)
         return kwargs
 
     def resources(self, **kwargs):
+        """Set named resources to the pyscript
+
+        Wrap this function around your rule resources. Be sure to include a double
+        asterisk before the function to unpack the dict, e.g.
+        `**pyscript.resources(...)`
+
+        Returns
+        -------
+        Dict
+            Dict of name, value pairs. This should be unpacked using a double asterisk
+        """
         self._resources = ScriptDict(**kwargs)
         return kwargs
 
     def wildcards(self, **kwargs):
+        """Set named wildcards to the pyscript
+
+        Wrap this function around your rule wildcards. Be sure to include a double
+        asterisk before the function to unpack the dict, e.g.
+        `**pyscript.wildcards(...)`
+
+        Returns
+        -------
+        Dict
+            Dict of name, value pairs. This should be unpacked using a double asterisk
+        """
         self._wildcards = ScriptDict(**kwargs)
         return kwargs
 
     def log(self, **kwargs):
+        """Set named logs to the pyscript
+
+        Wrap this function around your rule logs. Be sure to include a double
+        asterisk before the function to unpack the dict, e.g. `**pyscript.log(...)`
+
+        Returns
+        -------
+        Dict
+            Dict of name, value pairs. This should be unpacked using a double asterisk
+        """
         self._log = ScriptDict(**kwargs)
         return kwargs
 
@@ -82,6 +200,41 @@ class Pyscript:
         resources: PyscriptParam = None,
         log: PyscriptParam = None,
     ):
+        """Generate bash command to call python script.
+
+        Any data passed to the function will be passed to the script under the
+        appropriate variable names. Data names can also be provided here using the
+        parameters. Each parameter takes a list of variable names associated with the
+        data type. For example, if there are three params: x, y, and z, the params
+        argument here could be set to ["x", "z"]. This would cause x and z to be passed
+        to the script. These arguments take precedence over data passed through the
+        Pyscript methods.
+
+        Any data types not annotated via Pyscript methods or call parameters will be
+        passed to the script as a List.
+
+        Parameters
+        ----------
+        script : str
+            Path of the script to run. This, when combined with the snakemake_dir
+            provided to Pyscript, should form a fully resolved path to the script.
+        input : List of str, optional
+        output : List of str, optional
+        params : List of str, optional
+        wildcards : List of str, optional
+        resources : List of str, optional
+        log : List of str, optional
+
+        Returns
+        -------
+        str
+            Bash command to be passed to the snakemake shell directive
+
+        Raises
+        ------
+        FileExistsError
+            Raised if the specified script does not exist
+        """
         resolved_script = (self.snakefile_dir / script).resolve()
 
         if not Path(resolved_script).exists():
@@ -90,10 +243,10 @@ class Pyscript:
                 "Be sure to define paths relative to the app root, not the workflow "
                 "root."
             )
-        if self.env is None:
+        if self.venv is None:
             executable = "python"
         else:
-            executable = f"{self.env.get_venv} && {self.env.python_path}"
+            executable = f"{self.venv.get_venv} && {self.venv.python_path}"
         args = " ".join(
             [
                 _get_arg(arg, value)
@@ -132,8 +285,27 @@ def _parse_snakemake_arg(
     raise ParseError(f"Mixture of dict=args and listargs:\n\t{valuelist}")
 
 
-# pylint: disable=missing-class-docstring, redefined-builtin, too-many-arguments
+# pylint: disable=redefined-builtin, too-many-arguments
 class SnakemakeArgs:
+    """Class organizing the data passed from snakemake
+
+    This contains all the data, including inputs, outputs, params, etc, passed from
+    the Snakemake rule calling the script.
+
+    This class should not be initialized directly, but should be created through the
+    snakemake_args function
+
+    Attributes
+    ----------
+    input: List or Dict of paths
+    output: List or Dict of paths
+    params: List or Dict of str
+    wildcards : List or Dict of str
+    threads : int
+    resources : List or Dict of str
+    log : List or Dict of paths
+    """
+
     def __init__(
         self,
         input: List[str],
@@ -142,7 +314,7 @@ class SnakemakeArgs:
         wildcards: List[str],
         threads: str,
         resources: List[str],
-        log: str,
+        log: List[str],
     ):
         self.input = _parse_snakemake_arg(Path, input)
         self.output = _parse_snakemake_arg(Path, output)
@@ -150,7 +322,7 @@ class SnakemakeArgs:
         self.wildcards = _parse_snakemake_arg(str, wildcards)
         self.threads = int(threads)
         self.resources = _parse_snakemake_arg(str, resources)
-        self.log: Path = Path(log)
+        self.log = _parse_snakemake_arg(Path, log)
 
     def __eq__(self, obj: object):
         if not isinstance(obj, SnakemakeArgs):
@@ -171,6 +343,12 @@ class SnakemakeArgs:
 
 
 def snakemake_parser():
+    """Parser for snakemake args
+
+    Returns
+    -------
+    Argument Parser
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", nargs="*", default=[])
     parser.add_argument("--output", nargs="*", default=[])
@@ -178,12 +356,29 @@ def snakemake_parser():
     parser.add_argument("--wildcards", nargs="*", default=[])
     parser.add_argument("--threads", nargs="?", default=0, const=0)
     parser.add_argument("--resources", nargs="*", default=[])
-    parser.add_argument("--log", nargs="?", default="", const="")
+    parser.add_argument("--log", nargs="*", default=[])
     return parser
 
 
 def snakemake_args(
     argv: List[str] = None, parser: argparse.ArgumentParser = snakemake_parser()
 ):
+    """Snakemake args passed from snakemake rule
+
+    Parses the command line call made by Pyscript and returns an instance of
+    SnakemakeArgs for consumption in a python script.
+
+    Parameters
+    ----------
+    argv : List[str], optional
+        List of arguments to parse. Uses sys.argv[1:] by default
+    parser : argparse.ArgumentParser, optional
+        Argument parser to use. By default, uses the preconstructed snakemake_args
+        parser. This should be suitable for most applications.
+
+    Returns
+    -------
+    SnakemakeArgs
+    """
     args = parser.parse_args(argv)
     return SnakemakeArgs(**args.__dict__)
