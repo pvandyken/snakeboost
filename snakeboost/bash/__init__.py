9bdc3ffc520ff4fc3e6cb3168471ba5cdfc540f0
Peter Van Dyken <pvandyk2@uwo.ca>
2022-02-03 17:19:44 -0500

    Expose bash objects in snakeboost.bash

---
+++ snakeboost/bash/__init__.py
@@ -0,0 +1,58 @@
+__submodules__ = ["cmd", "statement"]
+
+# <AUTOGEN_INIT>
+from snakeboost.bash.cmd import (
+    DEBUG,
+    ShBlock,
+    ShCmd,
+    ShEntity,
+    ShPipe,
+    ShSetVariable,
+    ShSingleCmd,
+    ShStatement,
+    ShVar,
+    StringLike,
+    canonicalize,
+    echo,
+    find,
+    mkdir,
+    wc,
+)
+from snakeboost.bash.statement import (
+    BashWrapper,
+    Flock,
+    ShFor,
+    ShIf,
+    ShIfBody,
+    ShIfNot,
+    ShTry,
+    subsh,
+)
+
+__all__ = [
+    "BashWrapper",
+    "DEBUG",
+    "Flock",
+    "ShBlock",
+    "ShCmd",
+    "ShEntity",
+    "ShFor",
+    "ShIf",
+    "ShIfBody",
+    "ShIfNot",
+    "ShPipe",
+    "ShSetVariable",
+    "ShSingleCmd",
+    "ShStatement",
+    "ShTry",
+    "ShVar",
+    "StringLike",
+    "canonicalize",
+    "echo",
+    "find",
+    "mkdir",
+    "subsh",
+    "wc",
+]
+
+# </AUTOGEN_INIT>
